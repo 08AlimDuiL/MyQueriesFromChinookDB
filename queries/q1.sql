47bfdba810b6617e29e48887cf2cc616d0c0054b
Portnova Liudmila <portnovaliudmila@gmail.com>
2023-07-02 19:55:48 +0300

    Added more  queries for file q1

--- queries/q1.sql
+++ queries/q1.sql
@@ -11,19 +11,30 @@ WHERE InvoiceDate LIKE '%2012%' AND Total > 4.00
 ORDER BY Total DESC;
 
 -- 4.  Покажите дату продажи, адрес продажи, город в которую совершена продажа и стоимость покупки   не  равную 17.91. 
-   --  Присвоить названия столбцам: 
-    -- * InvoiceDate – ДатаПродажи;
-    -- *  BillingAddress – АдресПродажи;
-    -- *  BillingCity - ГородПродажи.
-
+--  Присвоить названия столбцам: InvoiceDate – Дата Продажи; BillingAddress – Адрес Продажи; BillingCity - Город Продажи.
+SELECT InvoiceDate AS 'Дата Продажи', BillingAddress AS 'Адрес Продажи',BillingCity AS 'Город Продажи', Total
+FROM chinook.invoice
+WHERE Total !=17.91
+ORDER BY Total DESC;
 
 -- 5.  Покажите фамилии и имена сотрудников компании, нанятых в 2003 году из города Калгари.
+SELECT LastName, FirstName, HireDate, City FROM chinook.employee
+WHERE HireDate LIKE '2003%' AND City LIKE 'Calgary';
 
 -- 6. Покажите канадские города, в которые были сделаны продажи в августе?
+SELECT BillingCity, InvoiceDate, BillingCountry
+FROM chinook.invoice
+WHERE BillingCountry LIKE 'Canada' AND InvoiceDate LIKE '%-08-%' ;
 
 -- 7.  Покажите Фамилии и имена работников, нанятых в октябре?
+SELECT LastName, FirstName, HireDate  FROM chinook.employee
+WHERE HireDate LIKE '%-10-%';
 
--- 8.  Покажите фамилии и имена сотрудников, занимающих должность менеджера по продажам и ИТ менеджера. Решите задание двумя способами: 
-    -- * используя оператор IN
-    -- * используя логические операции
+-- 8.  Покажите фамилии и имена сотрудников, занимающих должность менеджера по продажам и ИТ менеджера. 
+-- Решите задание двумя способами: используя оператор IN; используя логические операции.
+SELECT LastName, FirstName, Title  FROM chinook.employee
+WHERE Title IN ( 'IT Manager', 'Sales Manager');
+    
+SELECT LastName, FirstName, Title  FROM chinook.employee
+WHERE Title LIKE 'IT Manager' OR Title LIKE 'Sales Manager';
 
