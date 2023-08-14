34c98deb3a4cff313ff517702a456b41b6d643a5
Liudmila Portnova <portnovaliudmila@gmail.com>
2023-08-14 10:55:46 +0300

    Add corrections and missing queries (#7)

--- queries/q1.sql
+++ queries/q1.sql
@@ -1,46 +1,79 @@
--- 1.  Покажите содержимое таблицы исполнителей (артистов).
-    SELECT * FROM artist;
+-- 1. Покажите содержимое таблицы исполнителей (артистов).
+    SELECT *
+    FROM artist;
 
--- 2.  Покажите фамилии и имена клиентов из города Лондон.
-    SELECT FirstName, LastName 
+-- 2. Покажите фамилии и имена клиентов из города Лондон.
+    SELECT
+        FirstName,
+        LastName 
     FROM customer
     WHERE City LIKE 'London';
 
--- 3.  Покажите продажи за 2012 год, со стоимостью продаж более 4 долларов.
-    SELECT InvoiceDate, Total  
+-- 3. Покажите продажи за 2012 год, со стоимостью продаж более 4 долларов.
+    SELECT
+        InvoiceDate,
+        Total  
     FROM chinook.invoice
     WHERE InvoiceDate LIKE '%2012%' AND Total > 4.00
     ORDER BY Total DESC;
 
--- 4.  Покажите дату продажи, адрес продажи, город в которую совершена продажа и стоимость покупки   не  равную 17.91. 
---  Присвоить названия столбцам: InvoiceDate – Дата Продажи; BillingAddress – Адрес Продажи; BillingCity - Город Продажи.
-    SELECT InvoiceDate AS 'Дата Продажи', BillingAddress AS 'Адрес Продажи',BillingCity AS 'Город Продажи', Total
+/* 
+    4. Покажите дату продажи, адрес продажи, город в которую совершена продажа и стоимость 
+    покупки   не  равную 17.91. Присвоить названия столбцам:
+    InvoiceDate – Дата Продажи;
+    BillingAddress – Адрес Продажи;
+    BillingCity - Город Продажи.
+*/
+    SELECT
+        InvoiceDate AS 'Дата Продажи',
+        BillingAddress AS 'Адрес Продажи',
+        BillingCity AS 'Город Продажи',
+        Total
     FROM chinook.invoice
     WHERE Total !=17.91
     ORDER BY Total DESC;
 
--- 5.  Покажите фамилии и имена сотрудников компании, нанятых в 2003 году из города Калгари.
-    SELECT LastName, FirstName, HireDate, City 
+-- 5. Покажите фамилии и имена сотрудников компании, нанятых в 2003 году из города Калгари.
+    SELECT
+        LastName,
+        FirstName,
+        HireDate,
+        City 
     FROM chinook.employee
     WHERE HireDate LIKE '2003%' AND City LIKE 'Calgary';
 
 -- 6. Покажите канадские города, в которые были сделаны продажи в августе?
-    SELECT BillingCity, InvoiceDate, BillingCountry
+    SELECT
+        BillingCity,
+        InvoiceDate,
+        BillingCountry
     FROM chinook.invoice
-    WHERE BillingCountry LIKE 'Canada' AND InvoiceDate LIKE '%-08-%' ;
+    WHERE BillingCountry LIKE 'Canada' AND InvoiceDate LIKE '%-08-%';
 
--- 7.  Покажите Фамилии и имена работников, нанятых в октябре?
-    SELECT LastName, FirstName, HireDate
+-- 7. Покажите Фамилии и имена работников, нанятых в октябре?
+    SELECT
+        LastName,
+        FirstName,
+        HireDate
     FROM chinook.employee
     WHERE HireDate LIKE '%-10-%';
 
--- 8.  Покажите фамилии и имена сотрудников, занимающих должность менеджера по продажам и ИТ менеджера. 
--- Решите задание двумя способами: используя оператор IN; используя логические операции.
-    SELECT LastName, FirstName, Title  
+/* 
+    8. Покажите фамилии и имена сотрудников, занимающих должность менеджера по продажам и
+    ИТ менеджера. Решите задание двумя способами:
+    используя оператор IN;
+    используя логические операции.
+*/
+    SELECT
+        LastName,
+        FirstName,
+        Title  
     FROM chinook.employee
     WHERE Title IN ( 'IT Manager', 'Sales Manager');
     
-    SELECT LastName, FirstName, Title  
+    SELECT
+        LastName,
+        FirstName,
+        Title  
     FROM chinook.employee
     WHERE Title LIKE 'IT Manager' OR Title LIKE 'Sales Manager';
-
--- queries/q3.sql
+++ queries/q3.sql
@@ -3,27 +3,35 @@
     FROM chinook.track
     WHERE  Milliseconds = (SELECT MAX(Milliseconds) AS sec FROM chinook.track);
 
-    /*
-        2. Покажите название и длительность в секундах самой длинной песни применив округление по 
-        правилам математики.Столбец назвать sec.
-   */
-    SELECT `Name`, ROUND(Milliseconds/1000) AS sec
+/*
+    2. Покажите название и длительность в секундах самой длинной песни применив округление по 
+    правилам математики.Столбец назвать sec.
+*/
+    SELECT
+        `Name`,
+        ROUND(Milliseconds/1000) AS sec
     FROM chinook.track
-    WHERE  Milliseconds = (SELECT MAX(Milliseconds) FROM chinook.track);
+    WHERE Milliseconds = (SELECT MAX(Milliseconds) FROM chinook.track);
 
 -- 3. Покажите все счёт-фактуры, стоимость которых ниже средней.
-    SELECT * FROM chinook.invoice
+    SELECT * 
+    FROM chinook.invoice
     WHERE Total < (SELECT AVG(Total) FROM chinook.invoice);
 
 -- 4. Покажите счёт-фактуру с высокой стоимостью.
-    SELECT * FROM chinook.invoice
+    SELECT * 
+    FROM chinook.invoice
     WHERE Total = (SELECT MAX(Total) FROM chinook.invoice);
 
 -- 5. Покажите города, в которых живут и сотрудники, и клиенты.
-    SELECT City FROM chinook.customer
+    SELECT City 
+    FROM chinook.customer
     WHERE City IN (SELECT City FROM chinook.employee);
 
--- 6. Покажите имя, фамилию покупателя (номер 16), компанию и общую сумму его заказов. Столбец назовите sum.
+/*
+    6. Покажите имя, фамилию покупателя (номер 16), компанию и общую сумму его заказов.
+    Столбец назовите sum.
+*/
     SELECT 
         FirstName, 
         LastName, 
@@ -33,21 +41,21 @@
     FROM chinook.customer
     WHERE CustomerId = 16;
 
-    /*
-        7. Покажите сколько раз покупали треки группы Queen.  
-        Количество покупок необходимо посчитать по каждому треку. 
-        Вывести название, ИД трека и количество купленных треков группы Queen. 
-        Столбец назовите total.
-    */
+/*
+    7. Покажите сколько раз покупали треки группы Queen.  
+    Количество покупок необходимо посчитать по каждому треку. 
+    Вывести название, ИД трека и количество купленных треков группы Queen. 
+    Столбец назовите total.
+*/
     SELECT
 	    Name, 
         TrackId, 
-	    (SELECT COUNT(TrackId) FROM chinook.invoiceline WHERE TrackId = chinook.track.TrackId)  AS Total
+	    (SELECT COUNT(TrackId) FROM chinook.invoiceline WHERE TrackId=chinook.track.TrackId) AS Total
     FROM chinook.track
     WHERE Composer = "Queen";
 
 -- 8. Посчитайте количество треков в каждом альбоме. В результате вывести имя альбома и кол-во треков.
     SELECT 
         Title, 
-        (SELECT COUNT(*) FROM chinook.track WHERE track.AlbumId = chinook.album.AlbumId) AS count_track 
-        FROM chinook.album;
+        (SELECT COUNT(*) FROM chinook.track WHERE track.AlbumId=chinook.album.AlbumId) AS count_track 
+    FROM chinook.album;
